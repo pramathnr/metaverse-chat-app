a1c3655282099b9259c47ea0211c843435f91e9d
Pramath <pramath2010@gmail.com>
2022-01-05 23:00:14 +0530

    login with Moralis

---
+++ components/login.js
@@ -0,0 +1,27 @@
+import Image from "next/image"
+import Head from 'next/head'
+import { useMoralis } from 'react-moralis'
+
+function login() {
+
+    const {authenticate} = useMoralis();
+
+    return (
+        <div className="bg-black relative">
+            <Head>
+                <title>Metaverse Chat App</title>
+                <link rel="icon" href="https://iconape.com/wp-content/png_logo_vector/metaverse-etp-etp-logo.png" />
+            </Head>
+            {/* <h1>I am login</h1> */}
+            <div className="flex flex-col absolute z-50 items-center h-4/6 justify-center w-full space-y-4">
+                <Image src="https://wallpaperaccess.com/full/7547942.jpg" height={220} width={220} className="object-cover rounded-full"/>
+                <button onClick={authenticate} className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse">Login to the METAVERSE</button>
+            </div>
+            <div className="w-full h-screen">
+                <Image src="https://links.papareact.com/55n" layout="fill" objectFit="cover"/>
+            </div>
+        </div>
+    )
+}
+
+export default login
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import React from "react";
+import { MoralisProvider } from "react-moralis";
+import ReactDOM from "react-dom";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider appId={process.env.NEXT_PUBLIC_APP_ID} serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}>
+      <Component {...pageProps} />
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
